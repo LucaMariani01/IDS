1b75bbd31597303b6ed6a892d9a6c4ec065e6d0a
montecoMC <46907183+montecoMC@users.noreply.github.com>
2023-01-12 16:17:35 +0100

    test

---
+++ src/test/java/IDS/AziendaTest.java
@@ -0,0 +1,16 @@
+package IDS;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AziendaTest {
+
+    @Test
+    void getListaAdmin() {
+    }
+
+    @Test
+    void addAdmin() {
+    }
+}
