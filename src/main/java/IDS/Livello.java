55c5aeb6838a6ae823a6d45594709599644e7a3f
filipporeucci <reu.filippo@gmail.com>
2023-01-15 16:55:20 +0100

    Modifica classe myLivello e interfaccia Livello, eliminazione classe Requisito.java

--- src/main/java/IDS/Livello.java
+++ src/main/java/IDS/Livello.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public interface Livello {
     public int getNumero();
-    public ArrayList<Premio> getRequisitoEntrata();
+    public double getRequisitoEntrata();
     public ArrayList<Premio> catalogoPremi();
     public String getNome();
 }
