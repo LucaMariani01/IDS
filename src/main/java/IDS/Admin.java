9b97b67dc086a7b82ea2534147a63796cec66164
montecoMC <46907183+montecoMC@users.noreply.github.com>
2023-01-15 17:31:55 +0100

    MODIFICHE Admin.java

--- src/main/java/IDS/Admin.java
+++ src/main/java/IDS/Admin.java
@@ -1,16 +1,16 @@
 package IDS;
 
 public class Admin extends myCliente {
-    private final Azienda a ;
+    private final Azienda aziendaPartenenza;
 
     public Admin(int id, String nome, Azienda a) {
         super(id, nome);
 
         if (a == null) throw new NullPointerException();
-        this.a = a;
+        this.aziendaPartenenza = a;
     }
 
     public Azienda getA() {
-        return a;
+        return this.aziendaPartenenza;
     }
 }
