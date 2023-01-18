05657228895c31db1063ea75c15824a54aec3bd6
diego_pennesi <diegopennesi01@gmail.com>
2023-01-18 18:43:54 +0100

    Inizio sviluppo dashboard azienda

---
+++ src/main/java/IDS/DashBoardAzienda.java
@@ -0,0 +1,40 @@
+package IDS;
+
+import java.util.Scanner;
+
+public class DashBoardAzienda {
+
+
+    public int MenuAzienda()
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("1)CREA NUOVA CAMPAGNA ");
+        System.out.println("2)...");
+
+        int n = s.nextInt();
+
+        if (n < 1 || n> 2)
+        {
+            System.out.println("SCELTA NON VALIDA RIPROVA:");
+            return this.sceltaTipoCliente();
+        }return n ;
+    }
+    public int MenuAzienda()
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("1)CREA CAMPAGNA A PUNTI ");
+        System.out.println("2)CREA PROGRAMMMA A LIVELLI");
+        System.out.println("3)CREA CAMPAGNA CASHBACK");
+        System.out.println("4)CREA MEMBERSHIP ESCLUSIVA");
+        System.out.println("5)CREA PROGRAMMA COALIZIONE");
+
+        int n = s.nextInt();
+
+        if (n < 0 || n> 5)
+        {
+            System.out.println("SCELTA NON VALIDA RIPROVA:");
+            return this.sceltaCampagna();
+        }return n ;
+
+    }
+}
