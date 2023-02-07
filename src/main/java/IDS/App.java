5da9c191e156c1daff1ca264741e7fc2f5c6ac29
diego_pennesi <diegopennesi01@gmail.com>
2023-02-07 14:50:03 +0100

    aggiunta ciclo

--- src/main/java/IDS/App.java
+++ src/main/java/IDS/App.java
@@ -8,25 +8,29 @@ public class App {
         Menu m = new Menu();
         ClientePiattaforma c ;
         Optional<Azienda> aziendaLoggata = Optional.empty();
-
-        switch (m.sceltaTipoCliente())
-        {
-            case 1: //cliente
-                DashBoardCliente.menu();
-                break;
-            case 2: //azienda
-                switch (m.scelteLog()) {
-                    case 1 -> aziendaLoggata = DashBoardAzienda.login(); //login azienda
-                    case 2 -> DashBoardAzienda.registrazione(); //registrazione azienda
-                }
-                if(aziendaLoggata.isEmpty()) break; //se azienda non ha effettuato login esce
-                switch (m.menuCampagna()) {
-                    case 1:
-                        // TODO: 18/01/2023
+        int scelta;
+        do{
+            scelta=m.sceltaTipoCliente();
+            switch (scelta)
+            {
+                case 1: //cliente
+                    DashBoardCliente.menu();
                     break;
-                    default:System.out.println("AL PROSSIMO AGGIORNAMENTO");
-                }
-                break;
-        }
+                case 2: //azienda
+                    switch (m.scelteLog()) {
+                        case 1 -> aziendaLoggata = DashBoardAzienda.login(); //login azienda
+                        case 2 -> DashBoardAzienda.registrazione(); //registrazione azienda
+                    }
+                    if(aziendaLoggata.isEmpty()) break; //se azienda non ha effettuato login esce
+                    switch (m.menuCampagna()) {
+                        case 1:
+                            // TODO: 18/01/2023
+                            break;
+                        default:System.out.println("AL PROSSIMO AGGIORNAMENTO");
+                    }
+                    break;
+            }
+        }while(scelta!=0);
+
     }
 }
