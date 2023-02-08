3e4f8ed3c8c02a8895833bca4ff86e3383275137
montecoMC <46907183+montecoMC@users.noreply.github.com>
2023-02-08 15:43:11 +0100

    risoluzione date

--- src/main/java/IDS/DashBoardAzienda.java
+++ src/main/java/IDS/DashBoardAzienda.java
@@ -82,12 +82,13 @@ public class DashBoardAzienda {
     private void creaCampagnaScelta(int scelta,Azienda a) throws SQLException, ParseException {
         switch (scelta){
             case(1):
-                DbManager.creaCampagnaPunti(a.getId());
+                if(DbManager.creaCampagnaPunti(a.getId()).isPresent()) System.out.println("CAMPAGNA CREATA CON SUCCESSO");
+                else System.out.println("ERRORE NELLA CREAZIONE DELLA CAMPAGNA");
                 break;
             case(2)://campagna livelli
                 break;
             case (3): //campagna cashback
-                DbManager.creaCampagnaCashback(a.getId());
+               // DbManager.creaCampagnaCashback(a.getId());
                 break;
             case (4)://membership esclusiva
                 break;
