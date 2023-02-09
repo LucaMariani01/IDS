f0ac2c3690010df3f3304d630efd06eff136d3f1
diego_pennesi <diegopennesi01@gmail.com>
2023-02-09 10:30:19 +0100

    creo la classe per la rappreentazione del premio

---
+++ src/main/java/IDS/MyPremio.java
@@ -0,0 +1,23 @@
+package IDS;
+
+public class MyPremio implements Premio{
+
+    private int codice;
+
+    private String nome;
+
+    public MyPremio(int codice, String nome){
+        this.codice = codice;
+        this.nome = nome;
+    }
+
+    @Override
+    public int getCod() {
+        return codice;
+    }
+
+    @Override
+    public String getNome() {
+        return nome;
+    }
+}
